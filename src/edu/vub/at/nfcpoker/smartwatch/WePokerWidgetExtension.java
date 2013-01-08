eda2270397090becb070ef0ab680ee8eb07db6b5
Dries Harnie <dries.harnie@vub.ac.be>
2013-01-08 02:38:38 +0100

    fixt

--- src/edu/vub/at/nfcpoker/smartwatch/WePokerWidgetExtension.java
+++ src/edu/vub/at/nfcpoker/smartwatch/WePokerWidgetExtension.java
@@ -5,18 +5,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Handler;
+import android.text.TextPaint;
 import android.util.DisplayMetrics;
 import android.util.Log;
 
+import com.sonyericsson.extras.liveware.extension.util.ExtensionUtils;
 import com.sonyericsson.extras.liveware.extension.util.widget.WidgetExtension;
 
 public class WePokerWidgetExtension extends WidgetExtension {
 
 	public static final String UPDATE_ACTION = "edu.vub.at.nfcpoker.smartwatch.UPDATE";
+
+	private static final int WIDTH = 128;
+	private static final int HEIGHT = 110;
 
 	private Runnable mInfoTimeout;
 	private Handler mHandler = new Handler();
@@ -25,8 +31,15 @@ public class WePokerWidgetExtension extends WidgetExtension {
 
 	protected double mProbability = -1;
 
+	private final BitmapFactory.Options mBitmapOptions;
+
 	public WePokerWidgetExtension(Context context, String hostAppPackageName) {
 		super(context, hostAppPackageName);
+		
+        mBitmapOptions = new BitmapFactory.Options();
+        mBitmapOptions.inDensity = DisplayMetrics.DENSITY_DEFAULT;
+        mBitmapOptions.inTargetDensity = DisplayMetrics.DENSITY_DEFAULT;
+        
 		mFilter = new IntentFilter();
 		mFilter.addAction(UPDATE_ACTION);
 		
@@ -70,15 +83,34 @@ public class WePokerWidgetExtension extends WidgetExtension {
 	}	 
 
 	protected void updateWidget(boolean b) {
-        Bitmap bitmap = Bitmap.createBitmap(128, 110, Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888);
 
         // Set the density to default to avoid scaling.
         bitmap.setDensity(DisplayMetrics.DENSITY_DEFAULT);
         Canvas canvas = new Canvas(bitmap);
-        if (mProbability > 0) {
-        	canvas.drawColor(Color.GREEN);        	
-        } else {
-        	canvas.drawColor(Color.RED);
+        
+        canvas.drawColor(Color.WHITE);
+
+        Bitmap icon = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.background, mBitmapOptions);
+        canvas.drawBitmap(icon, (WIDTH - 90) / 2 + 2, (HEIGHT - 100) / 2, new Paint());
+        
+
+        if (mProbability >= 0) {
+        	final int textSize = 30;
+
+        	// Create default text paint
+        	TextPaint textPaint = new TextPaint();
+        	textPaint.setAntiAlias(true);
+        	textPaint.setTextAlign(Paint.Align.CENTER);
+        	textPaint.setTextSize(textSize);
+        	textPaint.setColor(Color.BLACK);
+        	
+        	Paint whitePaint = new Paint();
+        	whitePaint.setColor(Color.WHITE);
+        	whitePaint.setAlpha(192);
+			canvas.drawRect(0, (HEIGHT - textSize) / 2, WIDTH, (HEIGHT + textSize) / 2, whitePaint); 
+
+	        ExtensionUtils.drawText(canvas, String.format("%02d%%", Math.round(100 * mProbability)), WIDTH / 2, (HEIGHT + textSize) / 2 - 4, textPaint, WIDTH);
         }
         
         showBitmap(bitmap);
